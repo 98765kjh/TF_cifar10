aac45a2630d690ef5e2ec9bda6a816d18563168d
98765kjh <98765kjh@gmail.com>
2017-02-01 18:00:20 +0900

    read cifar batch

--- cifar.py
+++ cifar.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
+import numpy as np
 import cPickle
-import gzip
 
 def unpickle(file):
-    f = gzip.open(file, 'rb')
+    f = open(file, 'rb')
     data = cPickle.load(f)
     f.close()
     return data
 
-test = unpickle("CIFAR-10/cifar-10-python.tar.gz")
-print test
+test = unpickle("CIFAR-10-py/data_batch_1")
 
+arr = np.array(test['data'], np.uint8)
+arr = arr.reshape(10000, 3, 32, 32)
+
+
+#print test['labels']
